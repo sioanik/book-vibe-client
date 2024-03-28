e0d7aa5552f4c6ebcfe6531ff9cdd08c76013756
sioanik <sio.anik.007@gmail.com>
2024-03-28 17:44:50 +0600

    final v2

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className="mt-16 max-w-[90%] mx-auto hero bg-base-200">
             <div className="hero-content flex-col md:flex-row-reverse lg:flex-row-reverse gap-20">
-                <img src={banner} className="max-w-60 md:max-w-xs rounded-lg " />
+                <img src={banner} className="max-h-60 md:max-w-xs rounded-lg" />
                 <div className='flex flex-col items-center md:items-start'>
                     <h1 className="md:leading-[60px] max-w-md pb-6 text-2xl md:text-5xl font-bold">Books to freshen <br /> up your bookshelf</h1>
                     
