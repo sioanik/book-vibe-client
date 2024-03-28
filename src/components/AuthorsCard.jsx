2956e71ddb57ab72ffefaea40ae67af65f0eaa68
sioanik <sio.anik.007@gmail.com>
2024-03-28 17:47:32 +0600

    final v3 modified authors card

--- src/components/AuthorsCard.jsx
+++ src/components/AuthorsCard.jsx
@@ -5,7 +5,7 @@ const AuthorsCard = ({author}) => {
                 <div className="hero-content gap-14 flex-col lg:flex-row">
                     <img src={author.image} className="max-w-72 rounded-lg shadow-2xl" />
                     <div>
-                        <h1 className="text-2xl font-bold">{author.name}</h1>
+                        <h1 className="text-2xl font-bold text-center">{author.name}</h1>
                         <p className="py-6 text-lg">{author.about}</p>
                         <div className="flex gap-4">
                             <div className="bg-[#328EFF26] text-[#328effde] flex justify-center items-center rounded-3xl">
